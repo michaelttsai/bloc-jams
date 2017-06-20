16e14265d2c5e67e80b86c523a8248daa24aceb5
Michael Tsai <michael.t.tsai@gmail.com>
2017-06-20 09:41:04 -0500

    checkpoint 22 events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,28 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-     var points = document.getElementsByClassName('point');
-
+var animatePoints = function(points) {
     var revealPoint = function(counter) {
         points[counter].style.opacity = 1;
         points[counter].style.transform = "scaleX(1) translateY(0)";
         points[counter].style.msTransform = "scaleX(1) translateY(0)";
         points[counter].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
-     
+
     for (var i = 0; i < points.length; i++){
       revealPoint(i);
     }
  };
+
+ window.onload = function() {
+   if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+   var sellingPoints = document.getElementsByClassName('selling-points')[0];
+   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+   window.addEventListener('scroll', function(event) {
+     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+           animatePoints(pointsArray);
+       }
+  });
+  }
