a379eb748e4178d26e2a198b5a5c0ea904f753db
Michael Tsai <michael.t.tsai@gmail.com>
2017-06-30 13:52:50 -0400

    checkpoint 32 buzz library: completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,8 +1,22 @@
  var setSong = function(songNumber){
+   if (currentSoundFile) {
+           currentSoundFile.stop();
+       }
    currentlyPlayingSongNumber = parseInt(songNumber);
    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+   currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+      formats: [ 'mp3' ],
+      preload: true
+  });
+  setVolume(currentVolume);
  };
 
+ var setVolume = function(volume) {
+      if (currentSoundFile) {
+          currentSoundFile.setVolume(volume);
+      }
+  };
+  
  var getSongNumberCell = function(number){
    return $('.song-item-number[data-song-number="' + number + '"]')
  };
@@ -30,13 +44,19 @@
    		// Switch from Play -> Pause button to indicate new song is playing.
    		$(this).html(pauseButtonTemplate);
    		setSong(songNumber);
+       currentSoundFile.play();
       updatePlayerBarSong();
    	} else if (currentlyPlayingSongNumber === songNumber) {
    		// Switch from Pause -> Play button to pause currently playing song.
-   		$(this).html(playButtonTemplate);
-       $('.main-controls .play-pause').html(playerBarPlayButton);
-   		currentlyPlayingSongNumber = null;
-      currentSongFromAlbum = null;
+      if (currentSoundFile.isPaused()) {
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
+                currentSoundFile.play();
+            } else {
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();
+            }
    	}
    };
 
@@ -102,9 +122,8 @@
     var lastSongNumber = currentlyPlayingSongNumber;
 
     // Set a new current song
-    currentlyPlayingSongNumber = currentSongIndex + 1;
-    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
-
+    setSong(currentSongIndex + 1);
+    currentSoundFile.play();
     // Update the Player Bar information
     updatePlayerBarSong();
 
@@ -128,9 +147,8 @@ var previousSong = function() {
     var lastSongNumber = currentlyPlayingSongNumber;
 
     // Set a new current song
-    currentlyPlayingSongNumber = currentSongIndex + 1;
-    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
-
+    setSong(currentSongIndex + 1);
+    currentSoundFile.play();
     // Update the Player Bar information
     updatePlayerBarSong();
 
@@ -160,6 +178,8 @@ var previousSong = function() {
  var currentAlbum = null;
  var currentlyPlayingSongNumber = null;
  var currentSongFromAlbum = null;
+ var currentSoundFile = null;
+ var currentVolume = 80;
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
 
